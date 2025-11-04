a0af427ae931fbdb8785254a2a915565df22d6ca
AriaBot <aria-bot@noreply.github.com>
2025-11-04 23:24:18 +0000

    Updated aria to v0.9.20250918

--- Formula/aria.rb
+++ Formula/aria.rb
@@ -2,14 +2,14 @@ class Aria < Formula
   desc "The Aria Programming Language"
   homepage "https://egranata.github.io/aria/"
   license "Apache-2.0"
-  version "0.9.20251013"
+  version "v0.9.20250918"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/arialang/aria/releases/download/v0.9.20251013/aria-0.9.20251013-aarch64-apple-darwin-20251013222819.tgz"
-    sha256 "7c633b831114dd10454acbb601d5ef9b036c4a2e638302396bbed24cb75e6430"
+    url "https://github.com/arialang/aria/releases/download/v0.9.20250918/aria-0.9.20250918-aarch64-apple-darwin-20250918165231.tgz"
+    sha256 "23de60e3d0f044fd0268e40db44c7360c9a21713dd503ca527592c1d582b3483"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/arialang/aria/releases/download/v0.9.20251013/aria-0.9.20251013-x86_64-unknown-linux-gnu-20251013222823.tgz"
-    sha256 "0ee1391aee2fb97affe51723c55c932ca30d8ef9bf4e26adc59f711a6387dd72"
+    url "https://github.com/arialang/aria/releases/download/v0.9.20250918/aria-0.9.20250918-x86_64-unknown-linux-gnu-20250918165231.tgz"
+    sha256 "e329ecc4e7948239fbb015453e220aa3ab847f64851bfe85e480bce0c03ad827"
   else
     odie "Aria is not supported on your system (unsupported OS or CPU architecture)"
   end
